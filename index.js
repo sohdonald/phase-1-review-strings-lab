fff2f8306907340f8a0358b1bf219b7d17b53fdf
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-03-27 22:49:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+const currentUser = 'Bob';
+
+
+//this doesn't work
+//const welcomeMessage = "Welcome to Flatbook, currentUser";
+
+//const welcomeMessage = currentUser;
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
